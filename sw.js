e65daf649846c14f8f9c51f66a2847b31d04c21b
bhale79 <bhale@ipd-llc.com>
2026-03-27 11:22:40 -0500

    Bump service worker cache to mca-v51

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // fetches fresh copies in the background for next load.
 // NEVER caches Google API, OAuth, or Sheets calls.
 
-const CACHE_NAME = 'mca-v50';
+const CACHE_NAME = 'mca-v51';
 
 const SHELL_FILES = [
   './index.html',
