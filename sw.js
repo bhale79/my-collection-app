2c736285bc3345fb9912dbcf749efb59d934f4fa
bhale79 <bhale@ipd-llc.com>
2026-05-05 13:39:00 -0500

    Add MTH option to era dropdown in index.html (v0.9.213, mca-v224, ?v=256)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // fetches fresh copies in the background for next load.
 // NEVER caches Google API, OAuth, or Sheets calls.
 
-const CACHE_NAME = 'mca-v223';
+const CACHE_NAME = 'mca-v224';
 
 const SHELL_FILES = [
   './index.html',
