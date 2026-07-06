1de7e26c318ff92250ebceed92a0c7d021290b57
bhale79 <bhale@ipd-llc.com>
2026-07-05 19:31:23 -0500

    v0.9.711 sw.js: eBay query gets maker+name; Research typed quick-lookup

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // fetches fresh copies in the background for next load.
 // NEVER caches Google API, OAuth, or Sheets calls.
 
-const CACHE_NAME = 'mca-v721';
+const CACHE_NAME = 'mca-v722';
 
 const SHELL_FILES = [
   './index.html',
