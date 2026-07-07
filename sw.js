8286a7fb751bb3fa6b29356e9a98a6425e157287
bhale79 <bhale@ipd-llc.com>
2026-07-06 19:53:42 -0500

    v0.9.759 mca-v770

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // fetches fresh copies in the background for next load.
 // NEVER caches Google API, OAuth, or Sheets calls.
 
-const CACHE_NAME = 'mca-v769';
+const CACHE_NAME = 'mca-v770';
 
 const SHELL_FILES = [
   './index.html',
