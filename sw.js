825b3ade64f818229c5b936164ae1c1ef8d545f8
bhale79 <bhale@ipd-llc.com>
2026-07-08 10:17:07 -0500

    v0.9.770: FIX contact save (token retry in _ensureTab) + parser junk-proofing (sw.js)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // fetches fresh copies in the background for next load.
 // NEVER caches Google API, OAuth, or Sheets calls.
 
-const CACHE_NAME = 'mca-v780';
+const CACHE_NAME = 'mca-v781';
 
 const SHELL_FILES = [
   './index.html',
