05f920a34579eaca08fc26ad038e45510dc333a6
bhale79 <bhale@ipd-llc.com>
2026-07-08 08:40:22 -0500

    v0.9.767: Contacts store/cell/home phones + Title + smarter card parser (sw.js)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // fetches fresh copies in the background for next load.
 // NEVER caches Google API, OAuth, or Sheets calls.
 
-const CACHE_NAME = 'mca-v777';
+const CACHE_NAME = 'mca-v778';
 
 const SHELL_FILES = [
   './index.html',
